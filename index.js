eb3d4266db903d2b71484ac1661b3269b645e3b3
drewhouses <drewcasas@live.com>
2023-10-21 00:37:20 -0500

    added questions to array, installed inquirer

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 // TODO: Include packages needed for this application
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  "What is the title of your project?",
+  "Description",
+  "Would you like a table of contents?",
+  "Installation",
+  "Usage",
+  "Credits",
+  "License",
+  "Badges",
+  "Features",
+  "How to Contribute",
+  "Tests",
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
